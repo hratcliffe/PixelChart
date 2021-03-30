fab4194ced11f749ca42fe9f76e7253526c763ea
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-03-30 14:19:11 +0100

    Fit image to preview area

--- GUI/image_display.py
+++ GUI/image_display.py
@@ -20,11 +20,10 @@ class ImageHandler():
     sz = self.full_image.coreImage.size
     self.qimage = QImage(data, sz[0], sz[1], QImage.Format_RGB888) 
     pixmap = QPixmap.fromImage(self.qimage)
-
-    scale = 4
-    im_sz = [sz[0]*scale, sz[1]*scale]
-    print(im_sz)
-    pixmap = pixmap.scaled(im_sz[0], im_sz[1], qtc.Qt.KeepAspectRatio, qtc.Qt.FastTransformation)
+    port_sz = self.pane.viewport().size()
+    im_sz = qtc.QSize(sz[0], sz[1])
+    im_sz.scale(port_sz, qtc.Qt.KeepAspectRatio)
+    pixmap = pixmap.scaled(im_sz.width(), im_sz.height(), qtc.Qt.KeepAspectRatio, qtc.Qt.FastTransformation)
     self.image_hook.setPixmap(pixmap)
     self.image_hook.adjustSize()
     self.image_hook.show()
