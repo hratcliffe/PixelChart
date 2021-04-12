b792b202b84aca182f53de5a6eaf5ff48847eae0
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-04-12 15:45:52 +0100

    Add guides and add new style
    Dashed style as 'a' option

--- ColourHandling/interfaceRoutines.py
+++ ColourHandling/interfaceRoutines.py
@@ -1,9 +1,34 @@
 from . import imageExt
 from .detect import mergeColours
+from .replace import replaceColours, addGuide
+from .symbols import getUpscaling
 
 
 def reduceColours(image, n_cols, opt=False):
 
   new_image = mergeColours(image.getImage(opt), n_cols)
   image.setImage(new_image)
+
+
+def toSymbolicImage(image, guides=None):
+
+  # Guides specified as (spacing, style, colour) tuples or lists
+
+  new_image = imageExt(image.coreImage, image.coreImagePixels, image.colourMap)
+
+  bk_col = image.getColourWithMode((255,255,255))
+  fg_col = image.getColourWithMode((0,0,0))
+  guide_col = image.getColourWithMode((128,128,128))
+
+  new_image.setImage(replaceColours(image.coreImage, image.colourMap[0], bk_col, fg_col))
+  
+  scale = getUpscaling()
+  
+  #default guides are 1 and 10, all solid, grey and black
+  guides = [(1, 'l', guide_col), (10, 'l', fg_col)]
+  if guides:
+    for guide in guides:
+      addGuide(new_image.coreImage, guide[0]*scale, guide[1], guide[2])
+  
+  return new_image
 
--- ColourHandling/replace.py
+++ ColourHandling/replace.py
@@ -35,12 +35,74 @@ def replaceColours(originalImage, colourToSymbolMap, colour1, colour2 ):
     for j in range(sz[1]):
       symId = colourToSymbolMap[originalImage.getpixel((i,j))]
       symbol = symbols.getSymbol(symId)
-      for k in range(scale):
-        pixels[i*scale, j*scale + k] = colour2
-        pixels[i*scale+k, j*scale] = colour2
+#      for k in range(scale):
+#        pixels[i*scale, j*scale + k] = colour2
+#        pixels[i*scale+k, j*scale] = colour2
       
       for loc in symbol.locs:
         pixels[i*scale + loc[0]+1, j*scale + loc[1]+1] = colour2
+
+  # Enclosing box
+  for i in range(new_sz[0]):
+    pixels[i, 0] = colour2
+    pixels[i, new_sz[1]-1] = colour2
+  for j in range(new_sz[1]):
+    pixels[0, j] = colour2
+    pixels[new_sz[0]-1, j] = colour2
+    
         
   return imNew
+
+
+def addGuide(imageIn, spacing, style, colour):
+  """ Add a SINGLE set of guide lines with defined spacing, style and colour
+      E.g. adds lines at 0, spacing, spacing*2, spacing*3...
+      Image should be supplied as PIL image
+      Style is a single character
+      Colour should be suitable tuple for image mode
+      NB for 'c'ontrast style, range is assumed to be (0,255)"""
+
+  # Guide style None - do nothing
+  if(style == 'n') or style not in ['l', 'd', 'p', 'c', 'a']:
+    return
+
+  sz = imageIn.size
+  image = imageIn.load()
+
+  # Guides placed at multiples of spacing
+  for i in range(0, sz[0], spacing):
+    for j in range(0, sz[1], spacing):
+
+      # Set central pixel
+      image[i,j] = colour
+      # Also set additional pixels
+      if(style == 'l'):
+        line = int(spacing/2)
+      elif (style == 'p' or style == 'c'):
+        line = int(spacing/4)
+      if(style != 'd' and style !='a'):
+        for ii in range(-line, line+1):
+          try:
+            image[i+ii, j] = colour
+            image[i, j+ii] = colour
+          except:
+            pass
+      elif style =='a':
+        line = int(spacing/3)        
+        dist = int(spacing/2)
+        for ii in range(-dist, dist):
+          if int(ii/line)%2 == 0:
+            try:
+              image[i+ii, j] = colour
+              image[i, j+ii] = colour
+            except:
+              pass
+      # For contrast style, central pixel is contrasting colour
+      if(style == 'c'):
+        rev_colour = ()
+        for k in range(3):
+          rev_colour = rev_colour + ( 255-colour[k],)
+        for k in range(3, len(colour)):
+          rev_colour = rev_colour + (colour[k],)
+        image[i, j] = rev_colour
 
