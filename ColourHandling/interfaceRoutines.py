47ad6627faa0b0a5cfffb488a382671163d0964e
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-04-13 16:44:57 +0100

    Colour naming in LAB
    This is much more effective than trying to name in RGB even with
    simple heuristics.

--- ColourHandling/interfaceRoutines.py
+++ ColourHandling/interfaceRoutines.py
@@ -1,7 +1,8 @@
 from . import imageExt
 from .detect import mergeColours
-from .replace import replaceColours, addGuide, makeKeyItems
+from .replace import replaceColours, addGuide, makeKeyItems, makeDummy
 from .symbols import getUpscaling
+from .identify import nameColourLAB
 
 
 def reduceColours(image, n_cols, opt=False):
@@ -37,7 +38,17 @@ def getKey(image):
   bg_col = image.getColourWithMode((255,255,255))
   fg_col = image.getColourWithMode((0,0,0))
 
-  key = makeKeyItems(image.colourMap[0], bg_col, fg_col, mode=image.coreImage.mode)
+  # Create dummy image to simplify conversion to lab. Each pixel is one of the colours, in order left to right
   
-  return key
+  dummyImage = makeDummy(image.colourMap[0])
+  dPix = dummyImage.load()
+
+  key = makeKeyItems(image.colourMap[0], bg_col, fg_col, mode=image.coreImage.mode)
+
+  new_key = []
+  for item in key:
+    name = nameColourLAB(dPix[item[1], 0])
+    new_key.append((item[0], item[1], item[2], item[3], name))
+  
+  return new_key
 
