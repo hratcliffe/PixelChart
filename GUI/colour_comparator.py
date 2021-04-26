818e28c7aa3485cc80364acf9a83c85041326d68
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-04-26 16:01:55 +0100

    Add radio buttons and selection function
    Selected function is still a stub

--- GUI/colour_comparator.py
+++ GUI/colour_comparator.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QRadioButton
 from PyQt5 import uic
+from PyQt5.QtGui import QKeySequence
 from PyQt5 import QtCore as qtc
 
 from ColourHandling.interfaceRoutines import *
@@ -8,6 +9,7 @@ from XStitchHeuristics.colours import colourChart, colourItem
 from importlib_resources import files
 from PIL import ImageQt
 import re
+import random
 
 # UI file assumes at least 2 matches will be offered, which is true in general
 class ColourComparator(QWidget):
@@ -21,11 +23,16 @@ class ColourComparator(QWidget):
     self.setup_layout()
     self.show()
         
-  def setup_layout(self, inputColour=(200,0,0), nMatches=8):
+  def setup_layout(self, inputColour=None, nMatches=8):
     # Test - use some random colour
+    # NOTE: shortcut keys will only work with up to 9 matches!
+    
+    if not inputColour:
+      inputColour = tuple([int(random.random()*255) for i in range(3)])
 
     self.iColourLabel.setText(_subs(self.iColourLabel.text(), str(inputColour)))
     self.matchLabel.setText(_subs(self.matchLabel.text(), self.cChart.brand))
+    self.gridLayout.addWidget(self.matchLabel, 0, 2, 1, 3)
 
 
     r,g,b = inputColour
@@ -42,19 +49,28 @@ class ColourComparator(QWidget):
       self.gridLayout.addWidget(match, num+1, 1, 1, 2)
       match.adjustSize()
       label = QLabel("{} - {}".format(cMatch.num, cMatch.name))
-      self.gridLayout.addWidget(label, num+1, 3, 1, 1)
+      self.gridLayout.addWidget(label, num+1, 4, 1, 1)
+      rButt = QRadioButton(self)
+      rButt.setText(str(num+1)+')')
+      rButt.setShortcut(QKeySequence(str(num+1)))
+      if num == 0: rButt.setChecked(True)
+      rButt.clicked.connect(self.selected)
+      self.gridLayout.addWidget(rButt, num+1, 3)
 
 
     # Set input swatch and adjust to fit
     iSwatch = makeSwatch(inputColour)  
     swatch = iSwatch.toqpixmap()
-    swatch = swatch.scaled(200, 50*(len(matches)+2), qtc.Qt.IgnoreAspectRatio, qtc.Qt.FastTransformation)
+    swatch = swatch.scaled(250, 50*(len(matches)+2), qtc.Qt.IgnoreAspectRatio, qtc.Qt.FastTransformation)
 
     self.inputSwatch.setPixmap(swatch)
 
     # Add widget will remove and re-add
     self.gridLayout.addWidget(self.inputSwatch, 1, 0, len(matches), 2)
     self.inputSwatch.adjustSize()
+
+  def selected(self, val):
+    selection = int(self.sender().text().strip(')'))
 
 def _subs(text, sub):
   # Substitue sub into text where FIRST <> are found. If there are more than one pair, can call repeatedly and will subs from left to right
