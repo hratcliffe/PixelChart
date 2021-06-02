9cce58bc9cc8de8dbe9deb55aa6ac0bd9cd3336e
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-06-02 17:37:47 +0100

    Add serialise routines
    str methods for printing, repr for reproducing

--- SharedData/types.py
+++ SharedData/types.py
@@ -3,23 +3,48 @@ class ImageStatePayload():
     self.sz = sz
     self.n_cols = n_cols
 
+  def __str__(self):
+    return "ImageState: [sz={}, n_cols={}]".format(self.sz, self.n_cols)
+  def __repr__(self):
+    return "ImageStatePayload({},{})".format(self.sz, self.n_cols)
 
 class ImageChangePayload():
   def __init__(self, n_cols, opts={}):
     self.n_cols = n_cols
     self.opts = opts
 
+  def __str__(self):
+    return "ImageChange: [n_cols={}, opts={}]".format(self.n_cols, self.opts)
+  def __repr__(self):
+    return "ImageChangePayload({},{})".format(self.n_cols, self.opts)
+
 class PatternPayload():
   def __init__(self, filename, details):
     self.filename = filename
     self.details = details
+
+  def __str__(self):
+    return "Pattern: [filename={}, details={}]".format(self.filename, self.details)
+  def __repr__(self):
+    return "PatternPayload({},{})".format(self.filename, self.details)
 
 class ImageSizePayload():
   def __init__(self, wid, ht):
     self.width = wid
     self.height = ht
 
+  def __str__(self):
+    return "ImageSize: [width={}, height={}]".format(self.width, self.height)
+  def __repr__(self):
+    return "ImageSizePayload({},{})".format(self.width, self.height)
+
 class ColourRemapPayload():
   def __init__(self, brand, remap):
     self.brand = brand
     self.remap = remap
+
+  def __str__(self):
+    return "ColourRemap: [brand={}, remap={}]".format(self.brand, self.remap)
+  def __repr__(self):
+    return "ColourRemapPayload({},{})".format(self.brand, self.remap)
+
