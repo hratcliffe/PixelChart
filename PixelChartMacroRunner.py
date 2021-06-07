f9fb1c686149b8e6937dfd2b24999549c7bad890
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-06-07 17:18:14 +0100

    Basic hookup of runner
    Takes filename and macro file and runs resize and recolour steps.
    Currently finishes with a show command

--- PixelChartMacroRunner.py
+++ PixelChartMacroRunner.py
@@ -4,16 +4,46 @@ from PyQt5.uic import loadUi
 # This allows GUI for filename input and progress tracker etc
 
 from sys import argv
+from importlib_resources import files
 
 from Tracker.macro_system import MacroLoader
+from GUI.image_display import ImageHandler
+from GUI.colour_handling import ColourOptionsHandler
+
+
+class Ui(QMainWindow):
+  main_window_file = files('Tracker').joinpath('Main.ui')
+
+  def __init__(self):
+    super(Ui, self).__init__()
+    loadUi(self.main_window_file, self)
+    self.show()
+
+  def run_pressed(self):
+  
+    if self.runner:
+      self.imageH.show_image_from_file(self.input_file.text())
+      self.runner.run_file(self.macro_file.text(), showProgress=self.progress_check)
+
+    self.imageH.full_image.show()
 
 def main(args):
 
   app = QApplication(args)
-  #TODO Create window here once we have created UI
+  window = Ui()
 
-  runner = MacroLoader()
-  runner.run_file("test.json")
+  window.runner = MacroLoader()
+#  window.runner.run_file("test.json")
+  window.run_button.clicked.connect(window.run_pressed)
+
+  window.imageH = ImageHandler(window, has_display=False)
+  window.colourH = ColourOptionsHandler(window, has_display=False)
+
+  window.runner.runner.image_change_request.connect( window.imageH.on_image_change_request)
+  window.runner.runner.image_resize_request.connect( window.imageH.on_image_resize_request)
+
+
+  app.exec_()
 
 
 if __name__ == "__main__":
--- Tracker/macro_system.py
+++ Tracker/macro_system.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 
 from SharedData.types import *
+from GUI.image_display import ImageHandler
 
 import json
 
@@ -18,14 +19,18 @@ class MacroLoader:
   def run(self, items):
     self.runner.run(items)
 
-  def run_file(self, filename):
+  def run_file(self, filename, showProgress=False):
   
     with open(filename, 'r') as infile:
       items = json.load(infile)
     self.runner.run(items)
-
+    
 #Any saveable macro item needs corresponding signal in this class
 class MacroRunnerQt(QObject):
+
+  image_changed = pyqtSignal(ImageStatePayload, name="image_changed")
+  image_change_request = pyqtSignal(ImageChangePayload, name="image_change_request")
+  image_resize_request = pyqtSignal(ImageSizePayload, name="image_resize_request")
 
   def __init__(self):
     super(MacroRunnerQt, self).__init__() 
@@ -38,12 +43,15 @@ class MacroRunnerQt(QObject):
       try:
         if(item['name'] == "ImageStatePayload"):
           payload = ImageStatePayload(item['sz'], item['n_cols'])
+          self.image_changed.emit(payload)
         elif(item['name'] == "ImageChangePayload"):
-          payload = ImageChangePayload(item['n_cols'], item['opts'])
+          payload = ImageChangePayload(item['n_cols'], item['opts'])          
+          self.image_change_request.emit(payload)
         elif(item['name'] == "PatternPayload"):
           payload = PatternPayload(item['filename'], item['details'])
         elif(item['name'] == "ImageSizePayload"):
           payload = ImageSizePayload(item['width'], item['height'])
+          self.image_resize_request.emit(payload)
         elif(item['name'] == "ColourRemapPayload"):
           payload = ColourRemapPayload(item['brand'], item['remap'])
       except:
