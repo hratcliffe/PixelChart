0816c88d569420d206f854087fe401c1a7d2d1a4
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2021-06-10 17:16:45 +0100

    Add control functions to change loggers

--- Tracker/change_logger.py
+++ Tracker/change_logger.py
@@ -28,20 +28,45 @@ class ChangeLoggerQt(QObject):
   def write(self, filename):
     self.coreLogger.write(filename)  
 
+  def start(self):
+    self.coreLogger.start()
+
+  def pause(self):
+    self.coreLogger.pause()
+
+  def reset(self):
+    self.coreLogger.reset()
+
+  def debug_dump(self):
+    self.coreLogger.debug_dump()
+
+
 #Internal change logger 
 class ChangeLogger:
 
   def __init__(self):
     self.changeList = []
+    self.track = False
     
   def store(self, change):
     #Expects one of the Payload Types from SharedData - only needs __repr__ method tho'
-    print(change)
-    # TODO keep as objects or straight to repr??
-    # Are very lightweight objects so no great downside
-    self.changeList.append(change)
+    if self.track:
+      self.changeList.append(change)
     
   def write(self, filename):
      with open(filename, 'w') as outfile:
       json.dump(self.changeList, outfile)
       
+  def start(self):
+    self.track = True
+
+  def pause(self):
+    self.track = False
+
+  def reset(self):
+    self.track = False
+    self.changeList = []
+
+  def debug_dump(self):
+    for item in self.changeList:
+      print(str(item))
